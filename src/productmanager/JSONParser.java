08c5e0799c763541ff46cc23c9e2dda0e5900cc7
Marcus Schebye Skrvier <marcus@familienskriver.dk>
2022-02-08 13:53:40 +0100

    JSONParser ting

--- src/productmanager/JSONParser.java
+++ src/productmanager/JSONParser.java
@@ -1,7 +1,15 @@
 package productmanager;
 
+import java.util.ArrayList;
+
 public class JSONParser {
+    private String filepath = "";
 
+    public ArrayList<Product> read(){
+        return null;
+    }
 
-
+    public boolean write(Product product){
+        return false;
+    }
 }
