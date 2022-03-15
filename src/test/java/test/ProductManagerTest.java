1db98f0600d628a9ba08531c7418dfce9d3f84e6
Marcus Schebye Skrvier <marcus@familienskriver.dk>
2022-03-15 11:57:14 +0100

    ProductManagerTest virker nu

--- src/test/java/test/ProductManagerTest.java
+++ src/test/java/test/ProductManagerTest.java
@@ -6,74 +6,73 @@ import productmanager.ProductAttribute;
 import productmanager.ProductManager;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.*;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 class ProductManagerTest {
-
-
+    
+    private Product product;
+    private ProductManager productManager;
+    
     @BeforeAll
     static void initialize(){
-        System.out.println("============ ProductManager TEST SETUP ============");
-    }
-    PrintWriter print;
-    {
         try {
-            print = new PrintWriter("cheese.txt");
-            print.write("");
-            print.close();
-        } catch (FileNotFoundException e) {
+            FileWriter fw = new FileWriter("resources/cheese.txt");
+            fw.write("");
+            fw.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println("============ ProductManager TEST SETUP ============");
     }
-
+    
     @BeforeEach
     void setup(){
+        product = new Product();
+        productManager = new ProductManager("resources/cheese.txt");
         System.out.println("============ INITIALIZING ============");
     }
-    private final ProductManager productManager = new ProductManager("resources/cheese.txt");
-    private final Product product = new Product();
-
-
+    
     @Order(1)
     @Test
     void create() {
-
-    //creating product
-    assertTrue(productManager.create(product));
-    System.out.println("========== create() TEST DONE ============");
+        
+        //creating product
+        assertTrue(productManager.create(product));
+        System.out.println("========== create() TEST DONE ============");
     }
     
     @Order(2)
     @Test
     void createAll() {
-
-    //trying to createAll() using non-initialized, empty List
-    assertFalse(productManager.createAll(new ArrayList<Product>()));
-
-    ArrayList<Product> List = new ArrayList<>();
-
-    assertFalse(productManager.createAll(List));
-
-    //trying to createAll() after adding products to the List
-    List.add(product);
-    assertTrue(productManager.createAll(List));
-
-    System.out.println("========== createAll() TEST DONE ============");
+        
+        //trying to createAll() using non-initialized, empty List
+        assertFalse(productManager.createAll(new ArrayList<Product>()));
+        
+        ArrayList<Product> List = new ArrayList<>();
+        
+        assertFalse(productManager.createAll(List));
+        
+        //trying to createAll() after adding products to the List
+        List.add(product);
+        assertTrue(productManager.createAll(List));
+        
+        System.out.println("========== createAll() TEST DONE ============");
     }
     
     @Order(3)
     @Test
     void read() {
-
-    //trying to read a productId (UUID) using an empty String
-    assertNull(productManager.read(""));
-
-    //setting product UUID
-    product.set(ProductAttribute.UUID, "25");
-
-    //using read on a set product UUID
-    assertEquals(product.get(ProductAttribute.UUID), "25");
+        
+        //trying to read a productId (UUID) using an empty String
+        assertNull(productManager.read(""));
+        
+        //setting product UUID
+        product.set(ProductAttribute.UUID, "25");
+        
+        //using read on a set product UUID
+        assertEquals(product.get(ProductAttribute.UUID), "25");
         System.out.println("========== read() TEST DONE ============");
     }
     
@@ -82,14 +81,14 @@ class ProductManagerTest {
     void update() {
         //testing with nothing in the productArray
         assertFalse(productManager.update("46", ProductAttribute.UUID, "50"));
-
+        
         //adding product to productArray
         productManager.create(product);
         productManager.productArray.add(product);
-
+        
         //setting product UUID
         product.set(ProductAttribute.UUID, "30");
-
+        
         //testing if desired update occurred
         assertTrue(productManager.update("30", ProductAttribute.UUID, "50"));
         System.out.println("========== update() TEST DONE ============");
@@ -98,20 +97,20 @@ class ProductManagerTest {
     @Order(5)
     @Test
     void remove() {
-
+        
         //trying to remove product not in productArray
         assertFalse(productManager.remove("30"));
-
+        
         //adding product to productArray
         productManager.create(product);
         productManager.productArray.add(product);
-
+        
         //setting product UUID
         product.set(ProductAttribute.UUID, "30");
-
+        
         //trying to remove product in productArray, using faulty UUID
         assertFalse(productManager.remove("45"));
-
+        
         //trying to remove product in productArray, using actual UUID
         assertTrue(productManager.remove("30"));
         System.out.println("========== remove() TEST DONE ============");
@@ -122,48 +121,49 @@ class ProductManagerTest {
     void removeAll() {
         //Initializing String array of possible product UUIDs
         String[] ids = {"35", "42", "1", "364"};
-
+        
         //Initializing second String array of UUIDs, for test with 1 not matching
         String[] ids1 = {"35", "42", "1", "363"};
-
+        
         //creating additional product objects
         Product product1 = new Product();
         Product product2 = new Product();
         Product product3 = new Product();
-
+        Product product4 = new Product();
+        
         //trying to removeAll products in empty productArray
         assertFalse(productManager.removeAll(ids));
-
+        
         //adding products to productArray
-        productManager.create(product);
         productManager.create(product1);
         productManager.create(product2);
         productManager.create(product3);
-
-        productManager.productArray.add(product);
+        productManager.create(product4);
+        
         productManager.productArray.add(product1);
         productManager.productArray.add(product2);
         productManager.productArray.add(product3);
-
+        productManager.productArray.add(product4);
+        
         //setting product UUIDs
-        product.set(ProductAttribute.UUID, "35");
-        product1.set(ProductAttribute.UUID, "42");
-        product2.set(ProductAttribute.UUID, "1");
-        product3.set(ProductAttribute.UUID, "364");
-
+        product1.set(ProductAttribute.UUID, "35");
+        product2.set(ProductAttribute.UUID, "42");
+        product3.set(ProductAttribute.UUID, "1");
+        product4.set(ProductAttribute.UUID, "364");
+        
         //testing removeAll with an array containing 1 non-matching UUID
         assertFalse(productManager.removeAll(ids1));
-
+        
         //testing removeAll with an array of matching UUIDs
         assertTrue(productManager.removeAll(ids));
         System.out.println("========== removeAll() TEST DONE ============");
     }
-
+    
     @AfterEach
     void teardown(){
         System.out.println("============ TEARDOWN ============");
     }
-
+    
     @AfterAll
     static void ending(){
         System.out.println("============ TEST COMPLETED SUCCESSFULLY ============");
