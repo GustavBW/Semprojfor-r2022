22fed803c00f7d38fb81dffc6c243476eb732f0d
GustavBW <gustavbw@gmail.com>
2022-03-18 17:49:39 +0100

    Preparing Edit Mode for ProductGUI

--- src/main/java/GUI/ProductGUI.java
+++ src/main/java/GUI/ProductGUI.java
@@ -15,45 +15,85 @@ public class ProductGUI {
 
     private final Product product;
     private boolean isInEditMode = false;
-    private final ArrayList<Text> textBoxes;
+    private final List<Text> textBoxes;
     private final List<HBox> subContainers;
     private final HashMap<Text, ProductAttribute> textPattrMap;
+    private final HashMap<ProductAttribute, Text> pattrTextMap;
     private final VBox container;
 
     public ProductGUI(Product product){
         this.product = product;
         this.textPattrMap = new HashMap<>();
+        this.pattrTextMap = new HashMap<>();
         this.subContainers = new ArrayList<>();
-        textBoxes = generateGUI();
-
-        container = new VBox();
-        container.getChildren().addAll(textBoxes);
+        this.container = new VBox();
+        this.textBoxes = generateGUI();
     }
 
     private ArrayList<Text> generateGUI(){
-        ArrayList<Text> output = new ArrayList<>();
-
-        for(ProductAttribute pattr : ProductAttribute.values()){
-            HBox newHBox = new HBox();
-            Text newText = new Text(product.get(pattr));
-            newText.setDisable(true);
-            output.add(newText);
-
-            textPattrMap.put(newText,pattr);
+        if(!container.getChildren().isEmpty()){
+            container.getChildren().clear();
         }
 
+        //Title text
+        container.getChildren().add(new Text(product.get(ProductAttribute.NAME)));
+
+        ArrayList<Text> output = new ArrayList<>();
+        //This makes the IN_STOCK display correctly for GUI purposes. It shouldn't change anything anywhere else. But for good measure this change is reverted later
+        product.set(ProductAttribute.IN_STOCK, product.get(ProductAttribute.IN_STOCK).replaceAll(",","\n"));
+
+        //Generates a table of Hboxes in VBoxes of all product attributes
+        for(ProductAttribute pattr : ProductAttribute.values()){
+            HBox subContainer = new HBox();
+
+            Text attrText = new Text(product.get(pattr));
+            Text attrNameText = new Text(pattr.alias);
+
+            attrText.setDisable(true);
+            attrNameText.setDisable(true);
+
+            subContainer.getChildren().addAll(List.of(attrNameText,attrText));
+
+            subContainers.add(subContainer);
+            output.add(attrText);
+
+            textPattrMap.put(attrText,pattr);
+            pattrTextMap.put(pattr,attrText);
+        }
+
+        product.set(ProductAttribute.IN_STOCK, product.get(ProductAttribute.IN_STOCK).replaceAll("\n",","));
+        container.getChildren().addAll(subContainers);
         return output;
     }
 
-    public Node getContainer(){
+    public Node getContainerNode(){
         return container;
     }
 
     public boolean startEditMode(){
-
+        isInEditMode = true;
 
 
         return isInEditMode;
+    }
+    public boolean stopEditMode(){
+        isInEditMode = false;
+
+
+        return isInEditMode;
+    }
+
+    private Product getModProduct(){
+        Product output = new Product();
+
+        for(ProductAttribute pattr : ProductAttribute.values()){
+            output.set(pattr,pattrTextMap.get(pattr).getText());
+        }
+
+        return output;
+    }
+    public Product getProduct(){
+        return product;
     }
 
 }
