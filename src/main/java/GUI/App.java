6ad7f12cb95e9e0695eb48bedf274da2b119ca49
Fexelitche <gustavbw@gmail.com>
2022-03-18 12:06:15 +0100

    GUI package made

---
+++ src/main/java/GUI/App.java
@@ -0,0 +1,13 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        launch();
+    }
+}
