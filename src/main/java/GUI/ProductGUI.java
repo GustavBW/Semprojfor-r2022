a190da901fafa7b69be422b939d07135242fbd48
Fexelitche <gustavbw@gmail.com>
2022-03-18 12:13:51 +0100

    Super basic ProductGUI implimentation

--- src/main/java/GUI/ProductGUI.java
+++ src/main/java/GUI/ProductGUI.java
@@ -1,4 +1,41 @@
 package GUI;
 
+import javafx.scene.Node;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import productmanager.Product;
+import productmanager.ProductAttribute;
+
+import java.util.ArrayList;
+
 public class ProductGUI {
+
+    private final Product product;
+    private final ArrayList<Text> textBoxes;
+    private final VBox container;
+
+    public ProductGUI(Product product){
+        this.product = product;
+        textBoxes = generateGUI();
+        
+        container = new VBox();
+        container.getChildren().addAll(textBoxes);
+    }
+
+    private ArrayList<Text> generateGUI(){
+        ArrayList<Text> output = new ArrayList<>();
+
+        for(ProductAttribute pattr : ProductAttribute.values()){
+            output.add(new Text(product.get(pattr)));
+        }
+
+        return output;
+    }
+
+    public Node getContainer(){
+        return container;
+    }
+
+
+
 }
