18e636db44541976e511cc231f68f087beffed16
Fexelitche <gustavbw@gmail.com>
2022-03-18 16:16:44 +0100

    SubContainers

--- src/main/java/GUI/ProductGUI.java
+++ src/main/java/GUI/ProductGUI.java
@@ -1,21 +1,29 @@
 package GUI;
 
 import javafx.scene.Node;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import productmanager.Product;
 import productmanager.ProductAttribute;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ProductGUI {
 
     private final Product product;
+    private boolean isInEditMode = false;
     private final ArrayList<Text> textBoxes;
+    private final List<HBox> subContainers;
+    private final HashMap<Text, ProductAttribute> textPattrMap;
     private final VBox container;
 
     public ProductGUI(Product product){
         this.product = product;
+        this.textPattrMap = new HashMap<>();
+        this.subContainers = new ArrayList<>();
         textBoxes = generateGUI();
 
         container = new VBox();
@@ -26,9 +34,12 @@ public class ProductGUI {
         ArrayList<Text> output = new ArrayList<>();
 
         for(ProductAttribute pattr : ProductAttribute.values()){
+            HBox newHBox = new HBox();
             Text newText = new Text(product.get(pattr));
             newText.setDisable(true);
             output.add(newText);
+
+            textPattrMap.put(newText,pattr);
         }
 
         return output;
@@ -38,6 +49,11 @@ public class ProductGUI {
         return container;
     }
 
+    public boolean startEditMode(){
 
+
+
+        return isInEditMode;
+    }
 
 }
