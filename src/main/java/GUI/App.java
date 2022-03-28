c327d7cfdaa6108b8bbff74ea8b3cb75f5a584e7
Alexander Glering <algle11@student.sdu.dk>
2022-03-28 14:28:10 +0200

    APP GUI almost complete.

--- src/main/java/GUI/App.java
+++ src/main/java/GUI/App.java
@@ -1,16 +1,33 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.geometry.Point2D;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import productmanager.Product;
 import productmanager.ProductManager;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class App extends Application {
+public class App extends Application implements Initializable
+{
+
+    @FXML
+    private HBox btnHbox;
+    @FXML
+    private VBox btnVbox;
+    @FXML
+    private Pane productPane;
 
     public static ProductManager productManager = new ProductManager("resources/productsForApp.json");
     public static Point2D dim = new Point2D(1280,720);
@@ -19,23 +36,42 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUIapplication.fxml"));
+
+        prodGUIManager = new ProductGUIManager(productPane);
+
         Scene scene = new Scene(loader.load(), 1280, 720);
         stage.setTitle("PIM-1 GUI");
         stage.setScene(scene);
         stage.show();
 
+    }
 
-            //Scene scene = new Scene(content);
-            //stage.setScene(scene);
-            //stage.show();
+    private void addProductButtons() {
+        ArrayList<Product> products = productManager.getAllProducts();
+
+        for (Product p : products) {
+            ProductButton pB = new ProductButton(p, prodGUIManager);
+            btnVbox.getChildren().add(pB.getButton());
+        }
+    }
+
+    private void addFuncButtons() {
+        btnHbox.getChildren().add(new CreateNewProductButton().getButton());
+        //Add all the FuncButtons here
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        addProductButtons();
+        addFuncButtons();
+    }
+
+    //Comment-out above code (and remove "Initializable" interface for functioning original below
 
 
         /*Pane mainPane = new Pane();
         Pane prodGUIPane = new Pane();
         prodGUIManager = new ProductGUIManager(prodGUIPane);
-
-        addProductButtons(mainPane);
-        addFuncButtons(mainPane);
 
         mainPane.getChildren().add(prodGUIPane);
         Scene scene = new Scene(mainPane, dim.getX(),dim.getY());
@@ -45,6 +81,9 @@ public class App extends Application {
         stage.show();
         stage.requestFocus();
         stage.setOnCloseRequest(e -> stop());
+
+        addProductButtons(mainPane);
+        addFuncButtons(mainPane);
     }
 
     private void addFuncButtons(Pane node) {
@@ -67,8 +106,10 @@ public class App extends Application {
         }
 
         node.getChildren().add(vbox);
-         */
     }
+
+         */
+
 
     public synchronized void stop(){
         System.exit(69);
@@ -77,4 +118,6 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
     }
+
+
 }
