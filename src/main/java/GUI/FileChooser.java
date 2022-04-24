319df519749a4e0cff02a45ff99e60dd2b28d947
Tioman99 <90039319+Tioman99@users.noreply.github.com>
2022-04-24 20:25:07 +0200

    Update FileChooser.java

--- src/main/java/GUI/FileChooser.java
+++ src/main/java/GUI/FileChooser.java
@@ -3,21 +3,21 @@ import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
 import java.io.File;
 
-public class FileChooser { //This works just fine but won't compile in here.
-    /*
+public class FileChooser extends FuncButton {
+
+    public FileChooser() {super("Choose File");}
+    private String selectedFile;
+    @Override
+    public void onClicked(){
     JFileChooser chooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-        chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
         chooser.setDialogTitle("CHOOSE FILE TO LOAD");
 
     int returnValue = chooser.showOpenDialog(null);
         if (returnValue == JFileChooser.APPROVE_OPTION) {
         File selectedFile = chooser.getSelectedFile();
-        if (chooser.getSelectedFile().isFile()) {
-            System.out.println("You chose the file: " + selectedFile.getAbsolutePath());
-        }
-        else {
-            System.out.println("You chose a path: " + selectedFile.getAbsolutePath());
-        }
+
     }
-     */
+        //return selectedFile;
+    }
 }
