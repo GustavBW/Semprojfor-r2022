f607c98cfa82ea353328dbf02a617dde855fb32c
Marcus Skriver <marcus@familienskriver.dk>
2022-05-24 15:34:10 +0200

    Pulled in the launcher controller from the main repo
    This launcher is built to for the various groups to launch their programs from

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,16 @@
 module GUI {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
     
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    requires javafx.graphics;
     requires org.jetbrains.annotations;
     requires java.desktop;
 
+    opens dk.sdu.se_f22 to javafx.fxml;
     opens dk.sdu.se_f22.productmodule.management.GUI to javafx.fxml;
+    
+    exports dk.sdu.se_f22;
     exports dk.sdu.se_f22.productmodule.management.GUI;
 }
